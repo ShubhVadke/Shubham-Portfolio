ace3168361fde54101065efd3a38f3c6650c615c
Shubham Vadke <125117500+ShubhVadke@users.noreply.github.com>
2024-09-24 14:46:30 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -112,7 +112,7 @@ const Footer = () => {
 
         <div className="flex items-center justify-between pt-10 mb-8">
           <a href="/" className="logo reveal-up">
-            <img src="/images/icon.png" width={40} height={40} alt="Logo" />
+            <img src="/images/icon.png" width={45} height={45} alt="Logo" />
           </a>
 
           <p className="text-zinc-500 text-sm reveal-up">
