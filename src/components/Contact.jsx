c543f383084ba9f389dbdf1138daf30ca0770598
Shubham Vadke <125117500+ShubhVadke@users.noreply.github.com>
2024-11-17 21:14:26 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -80,9 +80,9 @@ const socialLinks = [
     href: "https://leetcode.com/u/shubhamvadke/",
     icon: (
       <svg
-        width="24"
-        height="24"
-        viewBox="0 0 22 22"
+        width="22"
+        height="22"
+        viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
